b5d6e06ba6e331ed03e4d5a2c721e3df7e4c8e65
Shreyansh-32 <thakurshreyansh0609@gmail.com>
2025-07-11 13:37:21 +0530

    Fix all lint errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', // The protocol (http or https)
+        hostname: 'images.unsplash.com', // The domain of your image
+        port: '', // Leave empty if no specific port is used
+        pathname: '/**', // Allow any path under this hostname
+      },
+    ],
+  },
 };
 
 export default nextConfig;

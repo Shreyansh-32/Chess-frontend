9ad67f9f8a1c53434180f4935c3a9e242f0b7051
Shreyansh-32 <thakurshreyansh0609@gmail.com>
2025-07-11 13:19:17 +0530

    Fix all lint errors

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,3 +1,25 @@
-import { PrismaClient } from "@prisma/client/extension";
+// lib/prisma.ts
 
-export const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client';
+
+// Add prisma to the NodeJS global type
+// This is to prevent multiple instances of PrismaClient in development
+// (hot-reloading can cause this)
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  // In development, store the PrismaClient on the global object
+  // to avoid creating new instances on hot reloads
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
